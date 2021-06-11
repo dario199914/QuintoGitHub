5235ffbe11627dc08a3dcf53dad4f5a679c38903
dario199914 <dario.diazmoreta.14@gmail.com>
2021-06-11 10:32:57 -0500

    Agregar archivos de proyecto.

---
+++ QuintoGitHub/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuintoGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Biennvenido a GITHUB");
+            Console.ReadKey();
+
+        }
+    }
+}
